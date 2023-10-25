3c0078b64c76b0e2a6956672e6f15d25c7152239
Sanarielsen <sanarielsen.dev@gmail.com>
2023-10-25 20:41:03 -0300

    feat(KRV-14): create route to update region
    Create route to update a existent region on database, including
    necessary configuration in respectfull controller, repository, route
    and security.

--- src/Controller/RegionController.php
+++ src/Controller/RegionController.php
@@ -4,14 +4,17 @@ namespace App\Controller;
 
 use App\Entity\Region;
 use Doctrine\ORM\EntityManagerInterface;
+use phpDocumentor\Reflection\Types\Integer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use function PHPUnit\Framework\isEmpty;
 
 class RegionController extends AbstractController
 {
-    #[Route('/region', name: 'app_region')]
+    #[Route('/region', name: 'korv_region_create')]
     public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
         $requestJSON = json_decode($request->getContent(), true);
@@ -26,5 +29,25 @@ class RegionController extends AbstractController
         $entityManager->flush();
 
         return $this->json(['status' => '200', 'message' => 'Região criada com sucesso.'], 200, ['Content-Type'=>'application/json; charset=utf-8']);
+    }
+
+    #[Route('/region/{id}', name: 'korv_region_update', methods: 'PUT')]
+    public function putRegion(Request $request, EntityManagerInterface $entityManager, int $id): Response
+    {
+        $regionExists = $entityManager->getRepository(Region::class)->findOneByIdExists($id);
+        if (!$regionExists) {
+            return $this->json(['status' => '404', 'message' => 'A região informada não existe.'], 404, ['Content-Type'=>'application/json; charset=utf-8']);
+        }
+        $requestJSON = json_decode($request->getContent(), true);
+        if (!array_key_exists("name", $requestJSON)) {
+            return $this->json(['status' => '422', 'message' => 'Erro ao modificar essa região.'], 422, ['Content-Type'=>'application/json; charset=utf-8']);
+        }
+
+        $region = $entityManager->getRepository(Region::class)->find($id);
+        $region->setName($requestJSON["name"]);
+
+        $entityManager->flush();
+
+        return $this->json(['status' => '200', 'message' => 'Região atualizada com sucesso.'], 200, ['Content-Type'=>'application/json; charset=utf-8']);
     }
 }
