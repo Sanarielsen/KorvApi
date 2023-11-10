fae21b509fa9f4943110bc60f602d455b62d1a02
Sanarielsen <sanarielsen.dev@gmail.com>
2023-11-10 02:19:55 -0300

    feat(KRV-27): update login route and create logout route
    Update login route because front-end needs a specific payload when user
    is authenticated. That is email, roles and token (continued created with
    JWT Levik following Sympony documentation) to valid that user in front.
    
    Create logout route because I have a lot of problems to understood how
    can I use login/logout in Sympony, but, now is clearly for me XD.

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -3,28 +3,52 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Security\Http\Attribute\CurrentUser;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 
 
 class LoginController extends AbstractController
 {
-    #[Route('/login', name: 'korv_api')]
-    public function index(#[CurrentUser] ?User $user): Response
+    #[Route('/login', name: 'korv_login')]
+    public function login(#[CurrentUser] ?User $authenticatedUser, Request $request, Security $security, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher, JWTTokenManagerInterface $JWTManager): Response
     {
-        if (null === $user) {
+        if (null === $authenticatedUser) {
             return $this->json([
+                'status' => 400,
                 'message' => 'missing credentials',
             ], Response::HTTP_UNAUTHORIZED);
         }
 
-        $token = ""; // somehow create an API token for $user
+        $token = $JWTManager->create($authenticatedUser);
 
         return $this->json([
-            'user' => $user->getUserIdentifier(),
+            'email' => $authenticatedUser->getEmail(),
+            'roles' => $authenticatedUser->getRoles(),
             'token' => $token,
         ]);
+    }
+
+    public function logout(Security $security): Response
+    {
+        if ($security->getUser() === null) {
+            return $this->json([
+                'status' => 202,
+                'message' => "The application don't have user authenticated to logout.",
+            ], Response::HTTP_ACCEPTED);
+        }
+        $security->logout(false);
+
+        return $this->json([
+            'status' => 200,
+            'message' => "The current user has successfully logged out of the application.",
+        ], Response::HTTP_OK);
     }
 }
