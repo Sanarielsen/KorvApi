9716c5f5e66158ab63dc00ec79dfd258c667fcb6
Sanarielsen <sanarielsen.dev@gmail.com>
2023-11-10 22:58:44 -0300

    feat(KRV-28): add a method to verify current client to that execution
    Add a method to verify every single execution inside of API to know
    where is reponsible to execute that route.
    
    Obs: We have updating in LoginController, but is only "clean code" like
    changes.

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -3,22 +3,17 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use Doctrine\ORM\EntityManagerInterface;
 use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\SecurityBundle\Security;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Security\Http\Attribute\CurrentUser;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
-
 
 class LoginController extends AbstractController
 {
     #[Route('/login', name: 'korv_login')]
-    public function login(#[CurrentUser] ?User $authenticatedUser, Request $request, Security $security, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher, JWTTokenManagerInterface $JWTManager): Response
+    public function login(#[CurrentUser] ?User $authenticatedUser, JWTTokenManagerInterface $JWTManager): Response
     {
         if (null === $authenticatedUser) {
             return $this->json([
