7eae43034ed39e48b649b892ca8a3b4bd5b57807
Sanarielsen <sanarielsen.dev@gmail.com>
2023-11-16 19:36:57 -0300

    feat(KRV-31): create controller user and migrate all methods relationed
    Create controller user (UserController) for tasks about user and migrate
    all created at now to relationed with user, for example, /register route
    is really /user POST just only.

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -27,41 +27,6 @@ class RegistrationController extends AbstractController
         $this->emailVerifier = $emailVerifier;
     }
 
-    #[Route('/register', name: 'app_register', methods: 'POST')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
-    {
-        $accessResponse = $this->userAuthenticatedVerifier->getHasAccessInCurrentRoute(['KORV_ADMIN']);
-        if ($accessResponse !== null) {
-            return $accessResponse;
-        }
-
-        try {
-            $user = new User();
-            $resultJson = json_decode($request->getContent(), true);
-
-            $user->setEmail($resultJson["username"]);
-            $user->setPassword(
-                $userPasswordHasher->hashPassword(
-                    $user,
-                    $resultJson["password"]
-                )
-            );
-            $user->setRoles($resultJson["roles"]);
-
-            $userFound = $entityManager->getRepository(User::class)->findUserByEmail($user->getEmail());
-            if ( $userFound !== [] ) {
-                return $this->json(['status' => '400', 'message' => 'Erro ao prosseguir com esse cadastro. Existem alguns campos que não passaram na validação do cadastro'], 200, ['Content-Type'=>'application/json; charset=utf-8']);
-            }
-
-            $entityManager->persist($user);
-            $entityManager->flush();
-
-            return $this->json(['status' => '200', 'message' => 'Funcionário cadastrado com sucesso.'], 200, ['Content-Type'=>'application/json; charset=utf-8']);
-        } catch (Exception $e) {
-            throw $this->createAccessDeniedException('Cannot create new user with current credentials');
-        }
-    }
-
     #[Route('/verify/email', name: 'app_verify_email')]
     public function verifyUserEmail(Request $request, TranslatorInterface $translator): Response
     {
---
+++ src/Controller/UserController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Security\EmailVerifier;
+use App\Security\UserAuthenticatedVerifier;
+use Doctrine\ORM\EntityManagerInterface;
+use PHPUnit\Exception;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    private UserAuthenticatedVerifier $userAuthenticatedVerifier;
+
+    public function __construct(UserAuthenticatedVerifier $userAuthenticatedVerifier, EmailVerifier $emailVerifier)
+    {
+        $this->userAuthenticatedVerifier = $userAuthenticatedVerifier;
+    }
+
+    #[Route('/user', name: 'korv_user_register', methods: 'POST')]
+    public function postRegister(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    {
+        $accessResponse = $this->userAuthenticatedVerifier->getHasAccessInCurrentRoute(['KORV_ADMIN']);
+        if ($accessResponse !== null) {
+            return $accessResponse;
+        }
+
+        try {
+            $user = new User();
+            $resultJson = json_decode($request->getContent(), true);
+
+            $user->setEmail($resultJson["username"]);
+            $user->setPassword(
+                $userPasswordHasher->hashPassword(
+                    $user,
+                    $resultJson["password"]
+                )
+            );
+            $user->setRoles($resultJson["roles"]);
+
+            $userFound = $entityManager->getRepository(User::class)->findUserByEmail($user->getEmail());
+            if ( $userFound !== [] ) {
+                return $this->json(['status' => '400', 'message' => 'Erro ao prosseguir com esse cadastro. Existem alguns campos que não passaram na validação do cadastro'], 200, ['Content-Type'=>'application/json; charset=utf-8']);
+            }
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->json(['status' => '200', 'message' => 'Funcionário cadastrado com sucesso.'], 200, ['Content-Type'=>'application/json; charset=utf-8']);
+        } catch (Exception $e) {
+            throw $this->createAccessDeniedException('Cannot create new user with current credentials');
+        }
+    }
+}
