2b49fab3497d4ec9c4995736f19be10712021175
Sanarielsen <sanarielsen.dev@gmail.com>
2023-11-18 22:29:38 -0300

    feat(KRV-17): update existing local
    Create route to update existing local using the new methods created in
    previously commit, example: makeResponsePostMessage and
    isBothHasTheSameProperties.

--- src/Controller/LocalController.php
+++ src/Controller/LocalController.php
@@ -67,4 +67,42 @@ class LocalController extends AbstractController
 
         return $this->responseMessage->makeResponsePostMessage(200, 'Local criado com sucesso.');
     }
+
+    #[Route('/local/:id', name: 'korv_local_put', methods: 'PUT')]
+    public function putLocal(Request $request, EntityManagerInterface $entityManager, int $id): Response
+    {
+        $accessResponse = $this->userAuthenticatedVerifier->getHasAccessInCurrentRoute(['KORV_ADMIN']);
+        if ($accessResponse !== null) {
+            return $accessResponse;
+        }
+
+        $requestJSON = json_decode($request->getContent(), true);
+        if ($requestJSON === null) {
+            return $this->responseMessage->makeResponsePostMessage(442, 'Não foi possível atualizar esse local, porque é necessário enviar um payload para essa requisição.');
+        }
+        if ( count($requestJSON) < 4 ) {
+            return $this->responseMessage->makeResponsePostMessage(442, 'Não foi possível atualizar esse local, porque faltou algumas informações nesse envio.');
+        }
+
+        $local = new Local();
+        $hasCorrectRequest = $this->propertiesValidation->isBothHasTheSameProperties($local, $requestJSON, ['id']);
+        if (!$hasCorrectRequest) {
+            return $this->responseMessage->makeResponsePostMessage(442, 'Não foi possível atualizar esse local, porque envio das informações dessa rota está incorreta.');
+        }
+
+        $regionRefer = $entityManager->getRepository(Region::class)->find($requestJSON['region']);
+        if ( $regionRefer === null ) {
+            return $this->responseMessage->makeResponsePostMessage(400, 'Não foi possível atualizar esse local, porque a região informada não existe.');
+        }
+
+        $local = $entityManager->getRepository(Local::class)->find($id);
+        $local->setName($requestJSON['name']);
+        $local->setType($requestJSON['type']);
+        $local->setAddress($requestJSON['address']);
+        $local->setRegion($regionRefer);
+
+        $entityManager->flush();
+
+        return $this->responseMessage->makeResponsePostMessage(200, 'Região atualizada com sucesso.');
+    }
 }
